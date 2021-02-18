c5e7ec0fbfb11c9e917660d21b95bc7a5bb62078
Yves Loeys <yves.loeys@gmail.com>
2021-02-18 13:02:30 -0800

    setup redux

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+  constructor(id, ownedId, title, imageUrl, description, price) {
+    this.id = id;
+    this.ownedId = ownedId;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+}
