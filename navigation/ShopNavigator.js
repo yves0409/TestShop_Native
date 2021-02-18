44e997a0e67334aa2a630c9661dc5ed596e43fd2
Yves Loeys <yves.loeys@gmail.com>
2021-02-18 13:52:49 -0800

    basic nav setup

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,22 @@
+import { Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Colors from "../constants/Colors";
+
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
