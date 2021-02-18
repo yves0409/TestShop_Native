45f37a92bed4795f0e854370ba22939f61fc0e8d
Yves Loeys <yves.loeys@gmail.com>
2021-02-18 14:35:36 -0800

    styling te item

---
+++ components/template.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const template = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default template;
