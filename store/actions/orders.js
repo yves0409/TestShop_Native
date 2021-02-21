dae26acef9a95fbf6c4213c99f6cba6f4ebcbcfe
Yves Loeys <yves.loeys@gmail.com>
2021-02-21 07:29:28 -0800

    sidedrawerMenu+adding icons

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,9 +1,8 @@
-export const ADD_ORDERS = "ADD_ORDERS";
+export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
   return {
-    type: ADD_ORDERS,
+    type: ADD_ORDER,
     orderData: { items: cartItems, amount: totalAmount },
   };
 };
-s;
