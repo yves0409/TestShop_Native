db53f9ec2b45c236d666de7d3ff39b114d9c2310
Yves Loeys <yves.loeys@gmail.com>
2021-02-21 17:16:13 -0800

    edit the buttons for userProduct

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,117 +1,13 @@
-// import React from "react";
-// import { View, Text, Image, StyleSheet, Button, Platform } from "react-native";
-// import {
-//   TouchableNativeFeedback,
-//   TouchableOpacity,
-// } from "react-native-gesture-handler";
-// import Colors from "../../constants/Colors";
-
-// const ProductItem = (props) => {
-//   let TouchableCmp = TouchableOpacity;
-
-//   if (Platform.OS === "android" && Platform.OS >= 21) {
-//     TouchableCmp = TouchableNativeFeedback;
-//   }
-
-//   return (
-//     <View style={styles.product}>
-//       <View style={styles.touchable}>
-//         <TouchableCmp onPress={props.onViewDetail} useForeground>
-//           <View>
-//             <View style={styles.imageContainer}>
-//               <Image style={styles.image} source={{ uri: props.image }} />
-//             </View>
-
-//             <View style={styles.detail}>
-//               <Text style={styles.title}>{props.title}</Text>
-//               <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-//             </View>
-
-//             <View style={styles.actions}>
-//               <Button
-//                 color={Colors.primary}
-//                 title="View Details"
-//                 onPress={props.onViewDetail}
-//               ></Button>
-//               <Button
-//                 color={Colors.primary}
-//                 title="To Cart"
-//                 onPress={props.onAddToCart}
-//               ></Button>
-//             </View>
-//           </View>
-//         </TouchableCmp>
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   product: {
-//     shadowColor: "black",
-//     shadowOpacity: 0.26,
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowRadius: 8,
-//     elevation: 5,
-//     borderRadius: 10,
-//     backgroundColor: "white",
-//     height: 300,
-//     margin: 20,
-//   },
-//   touchable: {
-//     overflow: "hidden",
-//     borderRadius: 10,
-//   },
-//   imageContainer: {
-//     width: "100%",
-//     height: "60%",
-//     borderTopLeftRadius: 10,
-//     borderTopRightRadius: 10,
-//     overflow: "hidden",
-//   },
-//   image: {
-//     width: "100%",
-//     height: "100%",
-//   },
-//   title: {
-//     fontFamily: "open-sans-bold",
-//     fontSize: 18,
-//     marginVertical: 2,
-//   },
-//   price: {
-//     fontFamily: "open-sans",
-//     fontSize: 14,
-//     color: "#888",
-//   },
-//   actions: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     alignItems: "center",
-//     height: "25%",
-//     paddingHorizontal: 20,
-//   },
-//   detail: {
-//     alignItems: "center",
-//     height: "15%",
-//     padding: 10,
-//   },
-// });
-
-// export default ProductItem;
-
 import React from "react";
 import {
   View,
   Text,
   Image,
   StyleSheet,
-  Button,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform,
 } from "react-native";
-
-import Colors from "../../constants/Colors";
 
 const ProductItem = (props) => {
   let TouchableCmp = TouchableOpacity;
@@ -123,7 +19,7 @@ const ProductItem = (props) => {
   return (
     <View style={styles.product}>
       <View style={styles.touchable}>
-        <TouchableCmp onPress={props.onViewDetail} useForeground>
+        <TouchableCmp onPress={props.onSelect} useForeground>
           <View>
             <View style={styles.imageContainer}>
               <Image style={styles.image} source={{ uri: props.image }} />
@@ -132,18 +28,7 @@ const ProductItem = (props) => {
               <Text style={styles.title}>{props.title}</Text>
               <Text style={styles.price}>${props.price.toFixed(2)}</Text>
             </View>
-            <View style={styles.actions}>
-              <Button
-                color={Colors.primary}
-                title="View Details"
-                onPress={props.onViewDetail}
-              />
-              <Button
-                color={Colors.primary}
-                title="To Cart"
-                onPress={props.onAddToCart}
-              />
-            </View>
+            <View style={styles.actions}>{props.children}</View>
           </View>
         </TouchableCmp>
       </View>
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { FlatList, Platform } from "react-native";
+import { FlatList, Platform, Button } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
 import HeaderButton from "../../components/UI/HeaderButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import * as CartActions from "../../store/actions/cart";
+import * as cartActions from "../../store/actions/cart";
+import Colors from "../../constants/Colors";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
   const dispatch = useDispatch();
+
+  const selectItemHandler = (id, title) => {
+    props.navigation.navigate("ProductDetail", {
+      productId: id,
+      productTitle: title,
+    });
+  };
 
   return (
     <FlatList
@@ -18,16 +26,25 @@ const ProductsOverviewScreen = (props) => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => {
-            props.navigation.navigate("ProductDetail", {
-              productId: itemData.item.id,
-              productTitle: itemData.item.title,
-            });
+          onSelect={() => {
+            selectItemHandler(itemData.item.id, itemData.item.title);
           }}
-          onAddToCart={() => {
-            dispatch(CartActions.addToCart(itemData.item));
-          }}
-        />
+        >
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="To Cart"
+            onPress={() => {
+              dispatch(cartActions.addToCart(itemData.item));
+            }}
+          />
+        </ProductItem>
       )}
     />
   );
