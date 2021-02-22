41f8e13522c3446673af493589407f0bb3799e89
Yves Loeys <yves.loeys@gmail.com>
2021-02-22 13:52:43 -0800

    outsourcing input to separate comp

---
+++ components/UI/Input.js
@@ -0,0 +1,55 @@
+import React, { useReducer } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const INPUT_CHANGE = "INPUT_CHANGE";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return { ...state, value: action.value, isValid: action.isValid };
+    default:
+      return state;
+  }
+};
+
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue ? props.initialValue : "",
+    isValid: props.initiallyValid,
+    touched: false,
+  });
+
+  const textChangeHandler = (text) => {
+    dispatch({ type: INPUT_CHANGE, value: text });
+  };
+  return (
+    <View style={styles.formControl}>
+      <Text style={styles.label}>{props.label}</Text>
+      <TextInput
+        {...props}
+        style={styles.input}
+        value={formState.inputValues.title}
+        onChangeText={textChangeHandler}
+      />
+      {!formState.inputValidities.title && <Text>{props.errorText}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formControl: {
+    width: "100%",
+  },
+  label: {
+    fontFamily: "open-sans-bold",
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+});
+
+export default Input;
