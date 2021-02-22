3f5120ed1bb0f9cf5b67329dbf3f62fd4c4d8f1c
Yves Loeys <yves.loeys@gmail.com>
2021-02-21 16:45:12 -0800

    fix in productReducer

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,7 +2,7 @@ import PRODUCTS from "../../data/dummy-data";
 
 const initialState = {
   availableProducts: PRODUCTS,
-  userProducts: PRODUCTS.filter((prod) => prod.ownerId === "u1"),
+  userProducts: PRODUCTS.filter((prod) => prod.ownedId === "u1"),
 };
 
 export default (state = initialState, action) => {
