6ff1fe15e78328b6bed968ea3f67e3ab3c756845
Yves Loeys <yves.loeys@gmail.com>
2021-02-23 11:00:28 -0800

    updating data id db

--- store/actions/products.js
+++ store/actions/products.js
@@ -4,6 +4,7 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 import Product from "../../models/product";
 
+//FETCHPRODUCTS Action
 export const fetchProducts = () => {
   return async (dispatch) => {
     try {
@@ -80,13 +81,29 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://native-shop-8ca66-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        header: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
